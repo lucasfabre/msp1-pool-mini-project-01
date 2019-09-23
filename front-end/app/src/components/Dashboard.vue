7a8b73d78e96c73e56d04016a83fb69cf354d386
wilfried <raynaudwilfried@gmail.com>
2019-09-23 21:14:13 +0200

    fix: apexchart

--- front-end/app/src/components/Dashboard.vue
+++ front-end/app/src/components/Dashboard.vue
@@ -42,18 +42,17 @@ export default {
       user_id: '',
       clocknotify: '',
       chartOptions: {
-          chart: {
-            id: 'vuechart-example'
-          },
-          xaxis: {
-            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-          }
+        chart: {
+          id: 'vuechart-example'
         },
-        series: [{
-          name: 'series-1',
-          data: [30, 40, 35, 50, 49, 60, 70, 91]
-        }]
-      }
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 35, 50, 49, 60, 70, 91]
+      }]
     }
   },
   methods: {
