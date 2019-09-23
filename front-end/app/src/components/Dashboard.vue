4088058d93591fbd6995e1409abcff3d23a5682e
wilfried <raynaudwilfried@gmail.com>
2019-09-23 17:41:03 +0200

    add: color donut

--- front-end/app/src/components/Dashboard.vue
+++ front-end/app/src/components/Dashboard.vue
@@ -37,7 +37,7 @@ export default {
       username: '',
       user_id: '',
       clocknotify: '',
-      sections: [{ label: 'Yellow', value: 25 }, { label: 'Green', value: 25 }, { label: 'Black', value: 25 }, { label: 'Grey', value: 25 }]
+      sections: [{ color: 'Yellow', value: 25 }, { color: 'Green', value: 25 }, { color: 'Black', value: 25 }, { color: 'Grey', value: 25 }]
     }
   },
   methods: {
