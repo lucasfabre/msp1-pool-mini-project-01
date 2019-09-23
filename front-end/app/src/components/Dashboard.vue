de253d5c4691d988cc8e9162c3dae3cc1f864c44
wilfried <raynaudwilfried@gmail.com>
2019-09-23 21:31:33 +0200

    fix: apexchart

--- front-end/app/src/components/Dashboard.vue
+++ front-end/app/src/components/Dashboard.vue
@@ -17,9 +17,7 @@
     </div>
     <br>
     <div id="chart">
-      <apexchart width="380" type="donut" :options="chartOptions" :series="series"></apexchart>
-    </div>
-    <div id="chart">
+      <apexchart width="380" type="donut" :options="options" :series="series"></apexchart>
     </div>
   </div>
 </template>
