4f4d7b1ad32bc5f7d38b8783fbc3d00bea2cd4e2
Mathilde Chabeau <mathilde.chabeau@epitech.eu>
2019-09-24 11:55:22 +0200

    test localhost

--- unit-test/delete_user.sh
+++ unit-test/delete_user.sh
@@ -1,5 +1,5 @@
 curl -X DELETE \
-  http://ec2-18-223-111-157.us-east-2.compute.amazonaws.com:4000/api/users/3 \
+  http://localhost:4000/api/users/3 \
   -H 'Content-Type: application/json' \
   -H 'Postman-Token: 0da95778-3cec-483d-9836-db42c4e965c4' \
   -H 'cache-control: no-cache' \
