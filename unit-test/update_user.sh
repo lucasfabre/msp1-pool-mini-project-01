ad573c345223ac34b6d243c61dfd6ab822004099
Mathilde Chabeau <mathilde.chabeau@epitech.eu>
2019-09-24 12:03:05 +0200

    Revert "test localhost"
    This reverts commit 4f4d7b1ad32bc5f7d38b8783fbc3d00bea2cd4e2.

--- unit-test/update_user.sh
+++ unit-test/update_user.sh
@@ -1,5 +1,5 @@
 curl -X PUT \
-  http://locahost:4000/api/users/2 \
+  http://ec2-18-223-111-157.us-east-2.compute.amazonaws.com:4000/api/users/2 \
   -H 'Content-Type: application/json' \
   -H 'Postman-Token: 610e5afc-dec6-40c2-9ccd-2b23ad8ab6b4' \
   -H 'cache-control: no-cache' \
