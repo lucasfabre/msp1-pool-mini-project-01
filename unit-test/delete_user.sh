efc0aa8c6431b215de7ec47e49823a0561ebfe64
Mathilde Chabeau <mathilde.chabeau@epitech.eu>
2019-09-25 13:38:00 +0200

    unit tests

--- unit-test/delete_user.sh
+++ unit-test/delete_user.sh
@@ -1,5 +1,5 @@
 curl -X DELETE \
-  http://ec2-13-59-172-229.us-east-2.compute.amazonaws.com:4000/api/users/3 \
+  http://ec2-13-59-172-229.us-east-2.compute.amazonaws.com:4000/api/users/1 \
   -H 'Content-Type: application/json' \
   -H 'Postman-Token: 0da95778-3cec-483d-9836-db42c4e965c4' \
   -H 'cache-control: no-cache' \
