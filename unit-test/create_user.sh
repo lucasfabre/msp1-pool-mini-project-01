1d8433908be91e1087420d41f83c2d48fe0a0356
Mathilde Chabeau <mathilde.chabeau@epitech.eu>
2019-09-25 17:35:42 +0200

    JPP

--- unit-test/create_user.sh
+++ unit-test/create_user.sh
@@ -1,14 +1,14 @@
-curl -X POST \
+export ID_USER=$(curl -X POST \
   http://ec2-13-59-172-229.us-east-2.compute.amazonaws.com:4000/api/sign_up \
   -H 'Content-Type: application/json' \
   -H 'Postman-Token: a7036f44-e4b7-4494-9d04-4af687c54d5e' \
   -H 'cache-control: no-cache,no-cache' \
   -d '{
     "user": {
-        "email": "unitTest@test.eu",
+        "email": "unitTest1@test.eu",
         "firstname": "Mathilde",
         "lastname": "Chabeau",
         "password": "plop",
         "roles": 0
     }
-}' --trace-ascii dumps.txt | jq ".data.id" > id_user && cat id_user
+}' --trace-ascii dumps.txt | jq ".data.id")
--- unit-test/get_clock.sh
+++ unit-test/get_clock.sh
@@ -1,4 +1,4 @@
 curl -X GET \
-  http://ec2-13-59-172-229.us-east-2.compute.amazonaws.com:4000/api/clocks/$(cat id_user) \
+  http://ec2-13-59-172-229.us-east-2.compute.amazonaws.com:4000/api/clocks/$(ID_USER) \
   -H 'Postman-Token: 4a9c5f27-6261-43df-af9c-7257347217f5' \
   -H 'cache-control: no-cache' --trace-ascii dumps.txt
