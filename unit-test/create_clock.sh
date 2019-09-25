dfb9f1ac1c676cd157df1f74682e23180478fa8f
Mathilde Chabeau <mathilde.chabeau@epitech.eu>
2019-09-25 14:17:05 +0200

    travis tests

---
+++ unit-test/create_clock.sh
@@ -0,0 +1,6 @@
+curl -X POST \
+  http://ec2-13-59-172-229.us-east-2.compute.amazonaws.com:4000/api/clocks/1 \
+  -H 'Content-Type: application/json' \
+  -H 'Postman-Token: 19a94fe7-b9ad-488d-bdb1-842e755b4c29' \
+  -H 'cache-control: no-cache,no-cache' \
+  -H 'session_jwt: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJKb2tlbiIsImV4cCI6MTU2OTQxNDE0NiwiaWF0IjoxNTY5NDA2OTQ2LCJpc3MiOiJKb2tlbiIsImp0aSI6IjJuM3FlamI3bHNqaTc1ZzFlYzAwMDEyaCIsIm5iZiI6MTU2OTQwNjk0NiwidXNlcl9pZCI6IjIifQ.IpouxBojrfrj8ApGE6akG4eCTTN2GezWLzGp6uWLnUo'
--- unit-test/create_user.sh
+++ unit-test/create_user.sh
@@ -5,7 +5,7 @@ curl -X POST \
   -H 'cache-control: no-cache,no-cache' \
   -d '{
     "user": {
-        "email": "test@epitech.eu",
+        "email": "mathilde.chabeau@epitech.eu",
         "firstname": "Mathilde",
         "lastname": "Chabeau",
         "password": "plop",
