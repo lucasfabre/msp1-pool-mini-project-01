fcecf5cb3b64119d9d52f3e28cdba836d5e7c4dc
Mathilde Chabeau <mathilde.chabeau@epitech.eu>
2019-09-26 14:43:30 +0200

    user V2

--- unit-test/create_user.sh
+++ unit-test/create_user.sh
@@ -5,7 +5,7 @@ curl -X POST \
   -H 'cache-control: no-cache,no-cache' \
   -d '{
     "user": {
-        "email": "MathildeTest14@unitTest.eu",
+        "email": "MathildeTest15@unitTest.eu",
         "firstname": "Mathilde",
         "lastname": "Chabeau",
         "password": "plop",
