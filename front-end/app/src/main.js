f60b25ad03509864d6a3e4e6384e05288579f8dc
wilfried <raynaudwilfried@gmail.com>
2019-09-27 10:28:55 +0200

    try: new version

--- front-end/app/src/main.js
+++ front-end/app/src/main.js
@@ -1,19 +1,38 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import VueLocalStorage from 'vue-localstorage'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import 'vuetify/dist/vuetify.min.css';
+import 'font-awesome/css/font-awesome.css';
+import Vuetify from 'vuetify';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import './styles/global.css';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueChartkick from 'vue-chartkick';
+import Chart from 'chart.js';
+import fullCalendar from 'vue-fullcalendar';
+import { setupComponents } from './config/setup-components';
+import swatches from 'vue-swatches';
+import "vue-swatches/dist/vue-swatches.min.css"
 import VueCookies from 'vue-cookies'
 import VueApexCharts from 'vue-apexcharts'
 import Raphael from 'raphael/raphael'
 global.Raphael = Raphael
+import VueLocalStorage from 'vue-localstorage'
 
 // JQUERY
 global.jQuery = require('jquery')
 let jQuery = global.jQuery
 window.jQuery = jQuery
 
+Vue.use(VueChartkick, { adapter: Chart });
+Vue.component('full-calendar', fullCalendar);
+Vue.component('swatches', swatches);
+Vue.use(VueAxios, axios);
+setupComponents(Vue);
+Vue.use(Vuetify);
 Vue.config.productionTip = false
 Vue.use(VueLocalStorage)
 Vue.use(VueCookies)
@@ -25,5 +44,10 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  data: {
+    themeColor: '#1D2939',
+    userEmail: 'admin@yopmail.com',
+    userPassword: '123456'
+  }
 })
--- front-end/app/src/router/index.js
+++ front-end/app/src/router/index.js
@@ -1,39 +1,113 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import CurrentWeather from '@/components/CurrentWeather'
-import SignIn from '@/components/SignIn'
-import SignUp from '@/components/SignUp'
-import Dashboard from '@/components/Dashboard'
-import UpdateAccount from '@/components/UpdateAccount'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+import Dashboard from '../pages/Dashboard.vue';
+import Social from '../pages/Social.vue';
+import Media from '../pages/Media.vue';
+import Snackbar from '../pages/Snackbar.vue';
+import Chart from '../pages/Chart.vue';
+import Mailbox from '../pages/Mailbox.vue';
+import Calendar from '../pages/Calendar.vue';
+import Login from '../pages/core/Login.vue';
+import Error from '../pages/core/Error.vue';
+
+
+
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/currentweather/:city',
-      name: 'City',
-      component: CurrentWeather
-    },
-    {
-      path: '/sign_in',
-      name: 'SignIn',
-      component: SignIn
-    },
-    {
-      path: '/sign_up',
-      name: 'SignUp',
-      component: SignUp
-    },
-    {
-      path: '/dashboard',
+      path: '/',
       name: 'Dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        breadcrumb: [
+          { name: 'Dashboard' }
+        ]
+      }
     },
     {
-      path: '/update_account',
-      name: 'UpdateAccount',
-      component: UpdateAccount
-    }
+      path: '/mailbox',
+      name: 'Mailbox',
+      component: Mailbox,
+      meta: {
+        breadcrumb: [
+          { name: 'Dashboard', href: 'Dashboard' },
+          { name: 'Mailbox' }
+        ]
+      }
+    },
+    {
+      path: '/snackbar',
+      name: 'Snackbar',
+      component: Snackbar,
+      meta: {
+        breadcrumb: [
+          { name: 'Dashboard', href: 'Dashboard' },
+          { name: 'Snackbar' }
+        ]
+      }
+    },
+    {
+      path: '/calendar',
+      name: 'Calendar',
+      component: Calendar,
+      meta: {
+        breadcrumb: [
+          { name: 'Dashboard', href: 'Dashboard' },
+          { name: 'Calendar' }
+        ]
+      }
+    },
+    {
+      path: '/social',
+      name: 'Social',
+      component: Social,
+      meta: {
+        breadcrumb: [
+          { name: 'Dashboard', href: 'Dashboard' },
+          { name: 'Social' }
+        ]
+      }
+    },
+    {
+      path: '/media',
+      name: 'Media',
+      component: Media,
+      meta: {
+        breadcrumb: [
+          { name: 'Dashboard', href: 'Dashboard' },
+          { name: 'Media' }
+        ]
+      }
+    },
+    {
+      path: '/chart',
+      name: 'Chart',
+      component: Chart,
+      meta: {
+        breadcrumb: [
+          { name: 'Dashboard', href: 'Dashboard' },
+          { name: 'Chart' }
+        ]
+      }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      meta: {
+        allowAnonymous: true
+      }
+    },
+    {
+      path: '/error',
+      name: 'Error',
+      component: Error,
+      meta: {
+        allowAnonymous: true
+      }
+    },
   ]
-})
+});
